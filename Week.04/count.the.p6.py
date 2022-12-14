0b4fb7a9e0a79fc35019881a1a53f0ebc86fb39e
AbhroChowdhury <109684820+AbhroChowdhury@users.noreply.github.com>
2022-12-13 23:12:12 -0700

    Update count.the.p6.py

--- Week.04/count.the.p6.py
+++ Week.04/count.the.p6.py
@@ -3,23 +3,28 @@ import sys
 InputFilename = "file.input.txt"
 
 
-def open_file(filename=InputFilename):
+def open_file(filename=InputFilename):  
     try:
-        f = open(filename, "r")
+        f = open(filename, "r")  # ooening the file in read mode
         return(f)
-    except FileNotFoundError:
+    except FileNotFoundError:      # error if the file is not found
         # FileNotFoundError is subclass of OSError
         print("File Not Found")
-        return(sys.stdin)
+        return(sys.stdin)  # returns the standard input stream so that the rest of the program can keep running
     except OSError:
-        print("OS Error")
+        print("OS Error") # error with open related to operating system
         return(sys.stdin)
 
 
+'''
+basically if the file is not opened, data will be read in as a command line input. This funciton just reads data from command line / file
+'''
+    
+    
 def safe_input(f=None, prompt=""):
     try:
         # Case:  Stdin
-        if f is sys.stdin or f is None:
+        if f is sys.stdin or f is None:  
             line = input(prompt)
         # Case:  From file
         else:
@@ -37,10 +42,10 @@ theCount = 0
 allWords = 0
 nonTarget = []
 cFlag = True
-inFile = open_file()        # New
+inFile = open_file()        # Open file will provide either the file or something from the command line
 while cFlag:
-    line, cFlag = safe_input(inFile)
-    if not cFlag:
+    line, cFlag = safe_input(inFile)  # same input will now read from that file which will pass through safe input to see if it 
+    if not cFlag: 
         break
     for w in line.split():
         allWords = allWords + 1
