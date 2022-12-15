3be98f0286c4eb346939a7bd9b8f773e098841b4
AbhroChowdhury <109684820+AbhroChowdhury@users.noreply.github.com>
2022-12-14 22:00:15 -0700

    Update lec09.p01.01.py

--- Week.05/lec09.p01.01.py
+++ Week.05/lec09.p01.01.py
@@ -1,7 +1,7 @@
 def map_bucket(x):
     buckets = [0.2, 0.4, 0.6, 0.8, 1.0]
     for i in range(len(buckets)):
-        if x < buckets[i]:
+        if x < buckets[i]: # if x is less than the bucket number, than we want to return the index of that bucket number
             return(i)
     return(-1)  # Error
 
