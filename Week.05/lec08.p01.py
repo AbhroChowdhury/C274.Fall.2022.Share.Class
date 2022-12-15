ed5291d1a04684a2f2d207ec0d245b588aecfe93
AbhroChowdhury <109684820+AbhroChowdhury@users.noreply.github.com>
2022-12-14 21:32:31 -0700

    Update lec08.p01.py

--- Week.05/lec08.p01.py
+++ Week.05/lec08.p01.py
@@ -1,16 +1,16 @@
-food = {}
+food = {}  # creating food called dictionary
 food["Toad in a Hole"] = "England"
 food["Pizza"] = "Italy"
 food["Xiao Long Bao"] = "China"
 food["Poutine"] = "Canada"
 food["Haggis"] = "Scotland"
-print("food:  ", food)
+print("food:  ", food)  
 print()
 
-food2 = { "Xiao Long Bao":"China", "Pizza":"Italy", "Poutine":"Canada", "Toad in a Hole":"England", "Haggis":"Scotland" }
+food2 = { "Xiao Long Bao":"China", "Pizza":"Italy", "Poutine":"Canada", "Toad in a Hole":"England", "Haggis":"Scotland" }  # another way of setting key value pairs in a dictionary
 print("food2: ", food2)
-for i in food2.keys():
+for i in food2.keys():  # loop through all the keys and print them 
     print(i)
 print()
-for i in food2.values():
+for i in food2.values():  # returns the values of the keys
     print(i)
