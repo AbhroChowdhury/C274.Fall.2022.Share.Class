d0e899c98a25beda8a7dcfc5c35578c4655d3550
AbhroChowdhury <109684820+AbhroChowdhury@users.noreply.github.com>
2022-12-14 21:57:40 -0700

    Update lec08.p03.partial.py

--- Week.05/lec08.p03.partial.py
+++ Week.05/lec08.p03.partial.py
@@ -1,5 +1,5 @@
 def biased_rolls(prob_list, s, n):
-    assert len(prob_list) >= 2, "Argument error"
+    assert len(prob_list) >= 2, "Argument error" # if assertion returns false, then assertionError will arise
     pass
 
 biased_rolls([1/2, 1/2], 0, 2)
